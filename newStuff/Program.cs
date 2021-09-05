74c9cace6a3e21301c686bfae8d6be194c9503be
Lashonda <lashondamiller007@gmail.com>
2021-09-05 13:45:18 -0400

    Data Types

--- newStuff/Program.cs
+++ newStuff/Program.cs
@@ -6,30 +6,18 @@ namespace newStuff
     {
         static void Main(string[] args)
         {
+            //different data types for variables
 
-            //in c# you tell it what kind of value you want the variable to store
-            //int stands for integer, args number without a decimal
-
-            string characterName = "Rick";
-            int characterAge;
-            characterAge = 50;
-            Console.WriteLine("There was a man named " + characterName);
-            Console.WriteLine(characterName + " was " + characterAge + " and silly");
-
-            //do this to change the value of a variable halfway, or at any point in a story
-            characterName = "Morty";
-            Console.WriteLine("There was something wrong with him");
-            Console.WriteLine(characterName + " got a big head");
+            string phrase = "Stuff";
+            char grade = 'C'; //this is for when you only want to have a single character, you also use single quotes
+            int age = -90;
+            //float, double, decimal are variable types that can store decimals, ranged from least to most accurate
+            double gpa = 3.9;
+            bool isMale = true; /*this is a boolean, which in most languaes are used as true or false*/
 
 
-            Console.ReadLine(); //press enter when you're done looking at the console when you use ReadLine
 
-            //two forward slashes to comment out single lines in c#
-
-            //press ctrl K + ctrl forward slash "/" to comment in and out as well, AND ctrl K + ctrl C
-
-            /* to coment out
-            multiple lines in c# */
+            Console.ReadLine();
         }
     }
 }
