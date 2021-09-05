ac6b2c8f0d8ad5d562f85cc825db198374eff903
Lashonda <lashondamiller007@gmail.com>
2021-09-04 20:20:11 -0400

    Drawing a Shape

--- newStuff/Program.cs
+++ newStuff/Program.cs
@@ -6,7 +6,20 @@ namespace newStuff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+
+            Console.ReadLine(); //press enter when you're done looking at the console when you use ReadLine
+
+            //two forward slashes to comment out single lines in c#
+
+            //press ctrl K + ctrl forward slash "/" to comment in and out as well
+
+            /* to coment out
+            multiple lines in c# */
         }
     }
 }
