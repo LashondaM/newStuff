65d267423d851fa3a142089508603f66d5867701
Lashonda <lashondamiller007@gmail.com>
2021-09-07 20:50:13 -0400

    Working With Numbers

--- newStuff/Program.cs
+++ newStuff/Program.cs
@@ -6,36 +6,42 @@ namespace newStuff
     {
         static void Main(string[] args)
         {
-            //c# also uses \n for "new-line" uses some of the same back slash attributes as JS in strings
-            //Console.WriteLine("Hot\nChocolate");
-            //Console.WriteLine("Hot \"Chocolate\"."); this back slash prints out the double quotes for a quote
+            //integers are whole numbers
 
-            //can also concatenate strings in c# like JS
-            // .Length tells us how many characters are in the string
-            //string phrase = "butter" + " is tasty with toast";
-            //Console.WriteLine(phrase.Length);
+            Console.WriteLine( 5 * 8 ); //you can do math
 
-            string phrase = "Taco Bell";
-            //this method changes all the character in the strings to uppercase, ToLower does the opposite
-            //Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine( 5 % 2 ); // this is a modulus operator, JS has this as well
 
-            //this is a boolean, tells us whether the value of the variable is true or false
-            //Console.WriteLine( phrase.Contains("Bell") );
+            Console.WriteLine( 2 + 8 * 7 ); // Order of operations
 
-            //this tells us what each character is inside the string when we use a number inside the brackets
-            //Console.WriteLine( phrase[6] );
+            Console.WriteLine( (4+2) / 2 ); // You can change the order of operations
 
-            //IndexOf tells us the index position the value is located inside the string, where the value first starts
-            //prints a value of -1 or 4 when you add a space inside the IndexOf method
-            //if you just type a letter, it gives the position of where that letter first appears
-            //-1 signifies a character is not there inside the string
-            //Console.WriteLine( phrase.IndexOf("o") );
+            Console.WriteLine( 5 / 2); // A math operation with int returns integers, even tho a decimal is needed
 
-            //this method grabs a part of the string we selct from the index and prints that part out
-            //you can also pass in how many characters you want it to grab after the starting point
-            Console.WriteLine( phrase.Substring(1, 7) );
+            Console.WriteLine( 5 / 2.0); // This however will return the decimal answer
+
+            int num = 9;
+            //num++; // you can increment numbers with variables, same with JS
+            num--; // you can also drecrement
+            Console.WriteLine(num);
+
+            /* Can also write Console.WriteLine( Math. ); to pull up different methods
+               that you can do with math in c# */
+
+            Console.WriteLine( Math.Abs(-49) ); // Abs - Absolute : this gives back the absolute value of a number
+
+            Console.WriteLine( Math.Pow(3, 4) ); // does three to the power of 4, also works with decimals
+
+            Console.WriteLine( Math.Sqrt(40) ); // Sqrt - squareroot
+
+            Console.WriteLine( Math.Max(53, 3) ); //spits out larger number
+            Console.WriteLine( Math.Min(-44, 10) ); // spits out smaller number
+
+            Console.WriteLine( Math.Round(10.4) ); // Rounds to the nearest number
+
 
             Console.ReadLine();
         }
     }
 }
+ 
