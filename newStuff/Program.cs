2bef8b98c482d6cbaeeabaaf3e10c2c5aa78282d
Lashonda <lashondamiller007@gmail.com>
2021-10-05 08:24:16 -0400

    While Loop and Do While

--- newStuff/Program.cs
+++ newStuff/Program.cs
@@ -6,47 +6,23 @@ namespace newStuff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetDay(6));
-
-            Console.ReadLine();
-        }
-        
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch (dayNum) // pass a value and check to see if it's equal to a whole bunch of stuff
+            // do while loop executes the code inside the loop first before checking the condition
+            int index = 6;
+            do
             {
-                case 0: // in the case dayNum is equal to 0, I want to do this down here, etc
-                    dayName = "Sunday";
-                    break; // put break to have c# stop checking for another case, if you want it to keep checking, don't use break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default: // use this to specify what to do if none of these cases are true
-                    dayName = "Invalid Day Number";
-                    break;
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5);
 
-            // Can also use an if statement to do these as well
 
-            }
+                //int index = 1;
+                //while (index <= 5)
+                //{
+                //    Console.WriteLine(index);
+                //    //index++; // loops infinitely because it's never told it needs to stop, the condition is never false
+                //}
 
-            return dayName;
+                Console.ReadLine();
         }
     }
 }
