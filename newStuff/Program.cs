2b4cb8b2698c540e852f21759a1fafd0335aaa55
Lashonda <lashondamiller007@gmail.com>
2022-06-01 13:23:53 -0400

    Object Methods

--- newStuff/Program.cs
+++ newStuff/Program.cs
@@ -6,40 +6,12 @@ namespace newStuff
     {
         static void Main(string[] args)
         {
-            // A constructor is a special method that you can put inside of a C# class that will get called whenever we create an object of that class
+            Student student1 = new Student("Bobby", "Comp Sci", 4.0);
 
-            // made objects - An instance of a class, like making an actual physical thing inside of our program after we set what type of data it will have in a class
+            Student student2 = new Student("Linda", "Underwater Basket Weaving", 2.7);
 
-            // new Book(); calls on the constructor method that we made in the Book class
-
-            // You can also pass paramaters into the constuctor inside of the Book class as well
-
-            // This is the old & more basic way to use constructors and make objects
-            /*
-            Book book1 = new Book("Chris");
-            book1.Title = "Harry Potter";
-            book1.Author = "JK Rowling";
-            book1.Pages = 400;
-
-            Book book2 = new Book("Rock");
-            book2.Title = "The Enemy";
-            book2.Author = "IDEK";
-            book2.Pages = 434;
-            */
-
-            //Console.WriteLine(book2.Author);
-
-            //===============================================
-            // This is the better way to make objects and use constructors for classes
-            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
-
-            book1.Author = "Final Destination";
-            Console.WriteLine(book1.Author);
-
-            Book book2 = new Book("The Enemy", "IDEK", 434);
-            Console.WriteLine(book2.Pages);
-
-            Book book3 = new Book();
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
 
             Console.ReadLine();
 
---
+++ newStuff/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newStuff
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public double Gpa { get; set; }
+
+        public Student(string name, string major, double gpa)
+        {
+            Name = name;
+            Major = major;
+            Gpa = gpa;
+        }
+
+        public bool HasHonors()
+        {
+            if(Gpa >= 3.5)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
