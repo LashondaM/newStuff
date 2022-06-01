33df53ea8cb56920a78abfd8776745d5cb41194d
Lashonda <lashondamiller007@gmail.com>
2022-05-31 23:49:26 -0400

    Constructors

--- newStuff/Book.cs
+++ newStuff/Book.cs
@@ -11,5 +11,26 @@ namespace newStuff
         public string Title { get; set; }
         public string Author { get; set; }
         public int Pages { get; set; }
+
+        // The constructor of this class
+        // You can also pass paramaters into the constuctor inside of the Book class as well
+        /* public Book(string name)
+        {
+            Console.WriteLine(name);
+        } */
+
+        // Made another constructor for another example of a better way to use constructors to make objects
+        // the "a" is just short for argument in this example
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            Title = aTitle;
+            Author = aAuthor;
+            Pages = aPages;
+        }
+
+        // You can also have more than one constructor
+        /* you can also set it up to have no params so then when you make an object in the main program class, you can either set an object with params or leave them w/o 
+        and the program won't mind */
+        public Book() { }
     }
 }
