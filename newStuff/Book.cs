f5c403a582280200f2d49a74a25013b374651bd8
Lashonda <lashondamiller007@gmail.com>
2022-05-31 23:09:21 -0400

    Classes & Objects

---
+++ newStuff/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newStuff
+{
+    internal class Book // Class was made to define what a book is by setting the data type of a book. The template for what a book is
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+    }
+}
--- newStuff/Program.cs
+++ newStuff/Program.cs
@@ -6,62 +6,19 @@ namespace newStuff
     {
         static void Main(string[] args)
         {
-            // SCENARIO 1
-            try
-            {
-                /* Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
+            // made objects - An instance of a class, like making an actual physical thing inside of our program after we set what type of data it will have in a class
+            
+            Book book1 = new Book();
+            book1.Title = "Harry Potter";
+            book1.Author = "JK Rowling";
+            book1.Pages = 400;
 
-                Console.WriteLine(num1 / num2); */
-            }
-            catch(Exception e) // This parameter is used to get more information about the error that occured
-            {
-                Console.WriteLine(e.Message); // This Tells us what went wrong when we have the Exception e print out the exception method
-            }
+            Book book2 = new Book();
+            book2.Title = "The Enemy";
+            book2.Author = "IDEK";
+            book2.Pages = 434;
 
-
-            // SCENARIO 2
-            try
-            {
-                /* Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine(num1 / num2); */
-            }
-            catch (DivideByZeroException e) // We can also specify specific exceptions we want this catch block to catch
-            {
-                Console.WriteLine(e.Message); //Now this will only catch exceptions whenever we try to divide by zero, but will ignore other different exceptions in the console we didn't specify here
-            } // but the other different exceptions will stop the program and show up in the code instead of the console
-
-
-            // SCENARIO 3
-            try
-            {
-                Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine(num1 / num2);
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch(FormatException e) // We can add more catches for different specific exceptions we want to see in the console
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                //optional
-                // Any code inside the finally block will be executed no matter what, even if there are exceptions. So if you still want to execute something, you'll put it in here
-                //can also search C# exception lists online
-            }
+            Console.WriteLine(book2.Author);
 
             Console.ReadLine();
 
